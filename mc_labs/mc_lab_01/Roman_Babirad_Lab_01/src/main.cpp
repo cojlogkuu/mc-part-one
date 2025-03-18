6392ea2f66c08c169ac3d1d640c31e3d6cc92a94
cojlogkuu <babiradrl@gmail.com>
2025-03-18 17:38:44 +0200

    clean code

--- mc_labs/mc_lab_01/Roman_Babirad_Lab_01/src/main.cpp
+++ mc_labs/mc_lab_01/Roman_Babirad_Lab_01/src/main.cpp
@@ -3,57 +3,75 @@
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
 
-const char *ssid = "dodkolox";
-const char *password = "qwerty1234";
+#define SSID "dodkolox"
+#define PASSWORD "qwerty1234"
 
-const uint8_t redLED = 2; // GPIO2 (D4)
-const uint8_t yellowLED = 14; // GPIO14 (D5)
-const uint8_t greenLED = 13; // GPIO13 (D7)
-const uint8_t button = 12; // GPIO12 (D6)
+typedef struct led_s {
+  uint8_t pin;
+  bool state;
+  led_s *next;
+  led_s *prev;
+} led_t;
 
-typedef enum {
-  RED,
-  YELLOW,
-  GREEN
-} LEDColor;
+typedef struct button_s {
+  uint8_t pin;
+  bool state;
+  bool wasPressed;
+} button_t;
 
-bool reversed = false;
-LEDColor currentLED = RED;
+led_t redLED = {D4, LOW, nullptr, nullptr}; // GPIO2
+led_t yellowLED = {D5, LOW, nullptr, nullptr}; // GPIO14
+led_t greenLED = {D7, LOW, nullptr, &yellowLED}; // GPIO13
+
+button_t button = {D6, LOW, false}; //GPIO12
+
+led_t *currentLED = &redLED;
+bool reversedOrder = false;
 uint32_t currentTime;
 uint16_t blinkInterval = 1000;
 uint32_t previousBlinkTime = 0;
 
-bool buttonState;
-bool buttonWasPressed = false;
 uint32_t pressStartTime = 0;
-uint16_t holdInterval = 1000;
+uint16_t holdInterval = 500;
 
 AsyncWebServer server(80);
 
 void handleHold(AsyncWebServerRequest *request) {
   Serial.println("WEB button was held");
-  reversed = true;
+  reversedOrder = true;
   request->send_P(200, "text/html", "ok");
 }
 
 void handleReleased(AsyncWebServerRequest *request) {
   Serial.println("WEB button was released");
-  reversed = false;
+  reversedOrder = false;
   request->send_P(200, "text/html", "ok");
 }
 
+void setupLEDOrder() {
+  redLED.next = &yellowLED;
+  redLED.prev = &greenLED;
+
+  yellowLED.next = &greenLED;
+  yellowLED.prev = &redLED;
+
+  greenLED.next = &redLED;
+  greenLED.prev = &yellowLED;
+}
+
 void pinSetup() {
-  pinMode(redLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
-  pinMode(greenLED, OUTPUT);
-  pinMode(button, INPUT_PULLUP);
+  pinMode(redLED.pin, OUTPUT);
+  pinMode(yellowLED.pin, OUTPUT);
+  pinMode(greenLED.pin, OUTPUT);
+  pinMode(button.pin, INPUT_PULLUP);
 }
 
 void serverSetup() {
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
   Serial.print("\nConnecting to wife");
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(5000);
+    delay(1000);
     Serial.print(".");
   }
 
@@ -62,67 +80,56 @@ void serverSetup() {
   server.on("/hold", HTTP_GET, handleHold);
   server.on("/release", HTTP_GET, handleReleased);
   server.begin();
-  Serial.print("\nConnected to WiFi: ");
+  Serial.println("Connected to WiFi: ");
   Serial.println(WiFi.localIP());
 }
 
-void turnOffLEDs() {
-  digitalWrite(redLED, LOW);
-  digitalWrite(yellowLED, LOW);
-  digitalWrite(greenLED, LOW);
+void lightLEDs() {
+  digitalWrite(redLED.pin, redLED.state);
+  digitalWrite(yellowLED.pin, yellowLED.state);
+  digitalWrite(greenLED.pin, greenLED.state);
 }
 
-void blinkLEDs() {
+void lightNextLED() {
   currentTime = millis();
 
   if (currentTime - previousBlinkTime >= blinkInterval) {
     previousBlinkTime = currentTime;
-    turnOffLEDs();
 
-    switch (currentLED) {
-      case 0: digitalWrite(redLED, HIGH);
-        break;
-      case 1: digitalWrite(yellowLED, HIGH);
-        break;
-      case 2: digitalWrite(greenLED, HIGH);
-        break;
-      default: ;
-    }
+    currentLED->state = LOW;
+    currentLED = reversedOrder ? currentLED->prev : currentLED->next;
+    currentLED->state = HIGH;
 
-    if (reversed) {
-      currentLED = (LEDColor)((currentLED - 1 + 3) % 3);
-    } else {
-      currentLED = (LEDColor)((currentLED + 1) % 3);
-    }
+    lightLEDs();
   }
 }
 
 void handleButtonHold() {
-  buttonState = digitalRead(button);
+  button.state = digitalRead(button.pin);
 
-  if (buttonState) {
-    if (!buttonWasPressed) {
-      buttonWasPressed = true;
+  if (button.state) {
+    if (!button.wasPressed) {
+      button.wasPressed = true;
       pressStartTime = millis();
     } else if (millis() - pressStartTime >= holdInterval) {
-      reversed = true;
+      reversedOrder = true;
     }
   } else {
-    if (buttonWasPressed) {
-      reversed = false;
+    if (button.wasPressed) {
+      reversedOrder = false;
     }
-    buttonWasPressed = false;
+    button.wasPressed = false;
   }
 }
 
 void setup() {
   Serial.begin(115200);
+  setupLEDOrder();
   pinSetup();
-  turnOffLEDs();
   serverSetup();
 }
 
 void loop() {
   handleButtonHold();
-  blinkLEDs();
+  lightNextLED();
 }
